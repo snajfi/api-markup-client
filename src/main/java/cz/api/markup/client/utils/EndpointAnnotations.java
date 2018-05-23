8c8eb07d6b15127157e8b908101e08db25f7b159
snajfi1 <Filip.Snajdr@hella.com>
2018-05-23 13:56:49 +0200

    Enum which holds endpoint annotations was created.

---
+++ src/main/java/cz/api/markup/client/utils/EndpointAnnotations.java
@@ -0,0 +1,43 @@
+package cz.api.markup.client.utils;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * TODO: Write documentation.
+ *
+ * @author Filip Snajdr, fsnajdr86(at)gmail.com
+ */
+public enum EndpointAnnotations {
+    ;
+
+    private static final HashMap<String, Class<? extends Annotation>> ANNOTATIONS = new HashMap<>();
+
+    static {
+        ANNOTATIONS.put("get", GET.class);
+        ANNOTATIONS.put("post", POST.class);
+        ANNOTATIONS.put("put", PUT.class);
+        ANNOTATIONS.put("delete", DELETE.class);
+    }
+
+    public static List<Class<? extends Annotation>> getAnnotationsForMethods(List<String> httpMethods) {
+
+        List<Class<? extends Annotation>> classes = new ArrayList<>();
+
+        for (String method : httpMethods) {
+            if (ANNOTATIONS.containsKey(method)) {
+                classes.add(ANNOTATIONS.get(method.toLowerCase()));
+            }
+        }
+
+        return classes;
+
+    }
+
+}
